d782776a6073ec9e1263af04a38f1a7271e58ea1
Timothy Gilbert <tsgilbert35@hotmail.com>
2023-08-16 16:51:58 -0700

    code cleanup

--- historical_data_import_APP.R
+++ historical_data_import_APP.R
@@ -80,15 +80,13 @@ server <- function(input, output) {
   
   observeEvent(input$create, {
     
-
+    ## creating log file to track progress
     sink("www/r_output.txt")
   
     ## pointing to VGS functions for batch data import
     source("Functions/VGS_functions_R.R", local = T)
     source("Functions/historical_data_importer.R", local = T)
     read_import_data(Protocol = input$Protocol, ServerKey = input$ServerKey, Protocol_2 = input$Protocol_2)
-    
-    #sink()
     
     ## close connections from local VGS db
     DBI::dbDisconnect(mydb)
@@ -105,47 +103,30 @@ server <- function(input, output) {
       data_log_output<- d_process()
       req(data_log_output)
       
-      
-      
       status_check<- substring(data_log_output[nrow(data_log_output),], 
                 first = nchar(data_log_output[nrow(data_log_output),])-24,
                 last = nchar(data_log_output[nrow(data_log_output),]))
       ## first one is NULL so needs to check 2nd
+      ## if reaches final output in function - sucessful = got past function checks
       if (status_check[2] == "**Batch Import Complete**") {
         shinyalert("Niceee!", "**Batch Import Complete**", type = "success")
-      } else
+      } else ## did not finish going through function for data import
         shinyalert("Oops!", "Something went wrong. Check log table or file", type = "error")
       
       data_log_output
       
     })
     
-
-    ## link to vgs / chaz voice ui pop ups
-    insertUI(selector = "#create",
-             where = "afterEnd",
-             ui = tagList(
-               tags$a(class="vgsLink", href="https://portal.vgs.arizona.edu/", "VGS Data Portal Link"),
-               tags$audio(src = "I love the VGS team.mp3", type = "audio/mp3", autoplay = T, controls = NA, style="display:none;"),
-               tags$p("See www/r_output.txt for data log file")
-             ))
-    
-    # if (data_log_output) {
-    #   
-    # }
-
-    
-    # # Show a modal when the button is pressed
-    # shinyalert("Oops!", "Something went wrong.", type = "error")
-    # 
-    # # Show a modal when the button is pressed
-    # shinyalert("Oops!", "Something went wrong.", type = "error")
+    # ## link to vgs / chaz voice ui pop ups
+    # insertUI(selector = "#create",
+    #          where = "afterEnd",
+    #          ui = tagList(
+    #            tags$a(class="vgsLink", href="https://portal.vgs.arizona.edu/", "VGS Data Portal Link"),
+    #            tags$audio(src = "I love the VGS team.mp3", type = "audio/mp3", autoplay = T, controls = NA, style="display:none;"),
+    #            tags$p("See www/r_output.txt for data log file")
+    #          ))
     
     })
-  
-
-
-
 }
 
 # Run the application
