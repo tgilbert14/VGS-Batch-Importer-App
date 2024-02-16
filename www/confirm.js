f6cba0f0ce9d3da734def8afd65ccfdd70cfefba
Timothy Gilbert <tsgilbert35@hotmail.com>
2024-02-16 16:27:12 -0700

    New animation for button push 'qaqc'

--- www/confirm.js
+++ www/confirm.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Mode Button
   $("#mode").on("change", async function(event) {
     // Check the state of the checkbox and change the title accordingly
     var title = $("#mode").is(":checked") ? "QA/QC mode engaged" : "QA/QC mode dis-engaged";
@@ -16,4 +17,24 @@ $(document).ready(function() {
       `
     });
   });
+  
+  // qaqc file button
+    $("#qaqc").on("change", async function(event) {
+    // Check the state of the checkbox and change the title accordingly
+    var title = $("#qaqc").is(":checked") ? "Using 'SpeciesReplace.xlsx' to update species!" : "Never mind...";
+    
+    Swal.fire({
+      title: title,
+      focusConfirm: false,
+      confirmButtonText: `meow`,
+      background: "orange",
+      backdrop: `
+        rgba(0,0,40,10)
+        url("/images/nyan-cat.gif")
+        left top
+        no-repeat
+      `
+    });
+  });
+  
 });
