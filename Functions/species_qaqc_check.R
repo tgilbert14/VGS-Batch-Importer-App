0ad30b81ddc70e1baea04b9e98adaa0582e17e20
Timothy Gilbert <tsgilbert35@hotmail.com>
2024-02-20 14:19:53 -0700

    adding SpeciesReplace file backup and creation

--- Functions/species_qaqc_check.R
+++ Functions/species_qaqc_check.R
@@ -1,6 +1,6 @@
 ## read in output log
 qaqc <- read_csv(paste0(app_path, "/www/r_output.txt"))
-View(qaqc)
+#View(qaqc)
 
 sp_conflicts <- unique(qaqc[grep("Species", qaqc[[1]], ignore.case = T), ])
 
@@ -20,10 +20,10 @@ file.show(paste0(app_path, "/www/Conflicts/sp_errors.xlsx"))
 # file.show(paste0(app_path,"/www/r_output.txt"))
 
 ## GC counts
-file_sum <- unique(qaqc[grep("Moving to File", qaqc[[1]]), ])
-gc_sum <- unique(qaqc[grep("ground cover points found", qaqc[[1]]), ])
-gc_check <- rbind(file_sum, gc_sum)
-write.xlsx(gc_check, paste0(app_path, "/www/Conflicts/gc_counts.xlsx"))
+#file_sum <- unique(qaqc[grep("Moving to File", qaqc[[1]]), ])
+gc_sum <- qaqc[grep("ground cover points", qaqc[[1]]), ]
+#gc_check <- rbind(file_sum, gc_sum)
+write.xlsx(gc_sum, paste0(app_path, "/www/Conflicts/gc_counts.xlsx"))
 ## open file
 # file.show(paste0(app_path,"/www/Conflicts/gc_counts_",site_name,".xlsx"))
 
@@ -57,3 +57,39 @@ species_count <- species_count %>%
 
 write.xlsx(species_count, paste0(app_path, "/www/Conflicts/species_count.xlsx"))
 file.show(paste0(app_path, "/www/Conflicts/species_count.xlsx"))
+
+## generating codes that need to be updated
+## Create "www/SpeciesReplace.xlsx" file with error species
+if (nrow(sp_errors) > 0) {
+  r=1
+  sp_codes_2_update<- list()
+  while (r < nrow(sp_errors)+1) {
+    
+    start_pos<- gregexpr("Species: ", sp_errors$`Species errors in VGS`[r])
+    end_pos<- gregexpr("not in", sp_errors$`Species errors in VGS`[r])
+
+    #print(substr(sp_errors$`Species errors in VGS`[r], start_pos[[1]][1]+9, end_pos[[1]][1]-2))
+    sp_codes_2_update[r]<- substr(sp_errors$`Species errors in VGS`[r], start_pos[[1]][1]+9, end_pos[[1]][1]-2)
+
+    r=r+1
+  }
+
+}
+sp_codes_2_update<- unique(sp_codes_2_update)
+sp_code_matrix<- matrix(sp_codes_2_update, nrow = length(sp_codes_2_update))
+sp_code_df<- as.data.frame(sp_code_matrix)
+
+sp_code_df <- sp_code_df %>% 
+  filter(nchar(sp_code_df$V1)>0) %>% 
+  arrange(V1)
+
+names(sp_code_df)<- "OldCodes"
+new_codes<- data.frame("NewCode"="")
+sp_update_file<- cbind(sp_code_df, new_codes)
+
+## update old file name and move, then save new one
+gu<- GUID(type = "sp")
+gu<- sub("\\$","",gu)
+gu<- sub("'","",gu)
+file.rename(from = paste0(app_path, "/www/SpeciesReplace.xlsx"), to = paste0(app_path, "/www/SpeciesReplaceBackups/SpeciesReplace",gu,".xlsx"))
+write.xlsx(sp_update_file, paste0(app_path, "/www/SpeciesReplace.xlsx"))
