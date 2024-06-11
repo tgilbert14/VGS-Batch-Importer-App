8907e879cbb1f852ac2e9f4e6a6e71369f5e1a27
Timothy Gilbert <tsgilbert35@hotmail.com>
2024-06-11 13:42:13 -0700

    Make a button to update newest SyncKey by transaction...

--- Functions/locatorSyncState_update.R
+++ Functions/locatorSyncState_update.R
@@ -1,13 +1,16 @@
 ##locatorSyncState_update
 ##updating SyncKEY, not syncSTATE
 
-# query .db for highest site.SyncState
-siteSyncCheck <- paste0("SELECT MAX(SyncKey) FROM locator")
+## this should be done after sites moved over to syncable...
+
+# query .db for highest SyncKey
+siteSyncCheck <- paste0("SELECT MAX(Key) FROM SyncTracking
+  where Status = 'LastTransaction'")
 site_sync.data <- dbGetQuery(mydb, siteSyncCheck)
 
-# update locator for that site.SyncState +1 for locator.SyncState
+# update locator for that max SyncKey +1 for locator.SyncKey
 locator_update <- paste0("Update locator
-Set SyncKey = ",as.numeric(site_sync.data$`MAX(SyncKey)`+1),"
+Set SyncKey = ",as.numeric(site_sync.data$`MAX(Key)` +1),"
 Where PK_Locator IS NOT NULL
 And DDLat IS NOT NULL
 And DDlong IS NOT NULL")
